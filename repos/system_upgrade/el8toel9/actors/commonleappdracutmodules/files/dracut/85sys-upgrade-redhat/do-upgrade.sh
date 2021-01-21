a10bd21a1debb89a6ef98e933b71807602ed2be0
Michal Reznik <mreznik@redhat.com>
2021-01-21 11:36:58 +0100

    Remove LEAPP3 HOME and use py3 ready Leapp

--- repos/system_upgrade/el8toel9/actors/commonleappdracutmodules/files/dracut/85sys-upgrade-redhat/do-upgrade.sh
+++ repos/system_upgrade/el8toel9/actors/commonleappdracutmodules/files/dracut/85sys-upgrade-redhat/do-upgrade.sh
@@ -8,8 +8,6 @@ fi
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 
 export LEAPPBIN=/usr/bin/leapp
-export LEAPPHOME=/root/tmp_leapp_py3
-export LEAPP3_BIN=$LEAPPHOME/leapp3
 
 export NEWROOT=${NEWROOT:-"/sysroot"}
 
@@ -39,7 +37,7 @@ do_upgrade() {
     # NOTE: in case we would need to run leapp before pivot, we would need to
     #       specify where the root is, e.g. --root=/sysroot
     # TODO: update: systemd-nspawn
-    /usr/bin/systemd-nspawn $NSPAWN_OPTS -D $NEWROOT /usr/bin/bash -c "mount -a; $LEAPP3_BIN upgrade --resume $args"
+    /usr/bin/systemd-nspawn $NSPAWN_OPTS -D $NEWROOT /usr/bin/bash -c "mount -a; $LEAPPBIN upgrade --resume $args"
     rv=$?
 
     # NOTE: flush the cached content to disk to ensure everything is written
@@ -60,7 +58,7 @@ do_upgrade() {
         # all FSTAB partitions. As mount was working before, hopefully will
         # work now as well. Later this should be probably modified as we will
         # need to handle more stuff around storage at all.
-        /usr/bin/systemd-nspawn $NSPAWN_OPTS -D $NEWROOT /usr/bin/bash -c "mount -a; /usr/bin/python3 $LEAPP3_BIN upgrade --resume $args"
+        /usr/bin/systemd-nspawn $NSPAWN_OPTS -D $NEWROOT /usr/bin/bash -c "mount -a; /usr/bin/python3 $LEAPPBIN upgrade --resume $args"
         rv=$?
     fi
 
